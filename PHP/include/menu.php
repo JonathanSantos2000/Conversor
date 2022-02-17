2db9a0ee905c0c1292c9f64a70d188256e03da51
Jonathan Costa <jonathancosta2100@gmail.com>
2022-02-17 14:26:50 -0300

    criando a pagina temperatura

---
+++ PHP/include/menu.php
@@ -0,0 +1,43 @@
+<div class='navigation'>
+    <ul>
+        <!-- Abrir menu -->
+        <li class='list active'>
+            <a href='#'>
+                <span class='icon'>
+                    <ion-icon name="menu-outline"></ion-icon>
+                </span>
+                <span class='title-menu'>MENU</span>
+            </a>
+        </li>
+
+        <!-- /* pagina inicial*/ -->
+        <li class='list'>
+            <a href='../PHP/index.php'>
+                <span class='icon'>
+                    <ion-icon name='home-outline'></ion-icon>
+                </span>
+                <span class='title-menu'>HOME</span>
+            </a>
+        </li>
+
+        <!-- /* pagina conversor tempo */ -->
+        <li class='list'>
+            <a href='../PHP/tempo.php'>
+                <span class='icon'>
+                    <ion-icon name='time-outline'></ion-icon>
+                </span>
+                <span class='title-menu'>TEMPO</span>
+            </a>
+        </li>
+
+        <li class='list'>
+            <a href='../PHP/temperatura.php'>
+                <span class='icon'>
+                    <ion-icon name="thermometer-outline"></ion-icon>
+                </span>
+                <span class='title-menu'>TEMPERATURA</span>
+            </a>
+        </li>
+
+    </ul>
+</div>
---
+++ PHP/temperatura.php
@@ -0,0 +1,56 @@
+<?php
+include '../PHP/include/header.php';
+?>
+
+<body>
+  <div class="flex-conteiner">
+    <div class="menu">
+      <?php
+      include '../PHP/include/menu.php';
+      ?>
+    </div>
+    <div class="contorno">
+      <div class="conteiner-cabecalho">
+        <h1 class="page-title">
+          Conversor de temperatura
+        </h1>
+        <p class="page-subtitle">
+          Converta as temperaturas para Celsius, Fahrenheit ou Kelvin
+        </p>
+        <hr>
+      </div>
+      <div id="conversor">
+        <input type="number" id="valor" size="2" placeholder="Insira o valor">
+        <div class="conversor">
+          <div class="number1">
+            <select name="select" id="weatherF">
+              <option value="">Selecionar</option>
+              <option value="celsius">Celsius</option>
+              <option value="fahrenheit">Fahrenheit</option>
+              <option value="kelvin">Kelvin</option>
+            </select>
+          </div>
+          <!-- sdasdasd -->
+          <div>
+            <h2>Para</h2>
+          </div>
+          <!-- saddasd -->
+          <div class="number2">
+            <select name="select" id="weatherS">
+              <option value="">Selecionar</option>
+              <option value="celsius">Celsius</option>
+              <option value="fahrenheit">Fahrenheit</option>
+              <option value="kelvin">Kelvin</option>
+            </select>
+          </div>
+
+          <br>
+        </div>
+        <button type="submit" onclick="Converter()">Converter</button>
+        <label for="valorConvertido" id="valorConvertido">O valor em _____ é de:</label>
+      </div>
+    </div>
+  </div>
+  <?php
+  include '../PHP/include/footer.php';
+  ?>
